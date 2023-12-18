61fb43e6ca175b4257be1c34d2bfb2ff8e3778b1
murad0510 <114216562+murad0510@users.noreply.github.com>
2023-12-18 16:47:57 +0400

    Update HomeController.cs

--- AspProjectZust.WebUI/Controllers/HomeController.cs
+++ AspProjectZust.WebUI/Controllers/HomeController.cs
@@ -4,9 +4,87 @@ namespace AspProjectZust.WebUI.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+         public IActionResult Birthday()
         {
             return View();
         }
+
+        public IActionResult Events()
+        {
+            return View();
+        }
+
+        public IActionResult Favorite()
+        {
+            return View();
+        }
+
+
+        public IActionResult Friends()
+        {
+            return View();
+        }
+
+        public IActionResult Groups()
+        {
+            return View();
+        }
+
+        public IActionResult HelpAndSupport()
+        {
+            return View();
+        }
+
+        public IActionResult LiveChat()
+        {
+            return View();
+        }
+
+        public IActionResult MarketPlace()
+        {
+            return View();
+        }
+
+        public IActionResult Messages()
+        {
+            return View();
+        }
+
+
+        public IActionResult MyProfile()
+        {
+            return View();
+        }
+
+        public IActionResult NewsFeed()
+        {
+            return View();
+        }
+
+        public IActionResult Notifications()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Setting()
+        {
+            return View();
+        }
+
+        public IActionResult Video()
+        {
+            return View();
+        }
+
+        public IActionResult Weather()
+        {
+            return View();
+        }
+    }
     }
 }
